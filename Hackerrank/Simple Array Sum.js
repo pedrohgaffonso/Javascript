4dbbbc8add949022df6650499161f97f8d6458b0
Pedro Henrique Garcia Affonso <pedrohgaffonso@gmail.com>
2024-11-05 16:51:08 -0300

    Delete itens api

--- Hackerrank/Simple Array Sum.js
+++
@@ -1,8 +0,0 @@
-function simpleArraySum(ar) {
-    let total = 0;
-    for (let i = 0; i < ar.length; i++) { 
-        total += ar[i];
-        console.log(ar[i]); 
-    }
-    return total; 
-}
--- Web-Scraping/README.md
+++
@@ -1,3 +0,0 @@
-# Web-Scraping
-
-Foi realizado a criação de um script para pegar as noticias secundarias de cada seção da pagina News do site [Yahoo Finance](https://finance.yahoo.com/news/).
